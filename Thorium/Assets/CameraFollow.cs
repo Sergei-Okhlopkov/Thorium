0674d11dd2f1b51ac58dffc1f847a44ad71da7d5
Sergei-Okhlopkov <s_okhlopkov@mail.ru>
2023-02-18 21:04:42 +0300

    player's follow camera added
    Camera follows player with smooth effect

---
+++ Thorium/Assets/CameraFollow.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform target;
+
+    public float smoothSpeed;
+
+    public Vector3 offset;
+
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+    }
+}
