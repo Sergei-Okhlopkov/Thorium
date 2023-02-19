ec98b3310185ae4944793ed11058897f6cb26ef6
Sergei-Okhlopkov <s_okhlopkov@mail.ru>
2023-02-19 18:55:28 +0300

    Added Enemy

---
+++ Thorium/Assets/EnemyCatchPlayer.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCatchPlayer : MonoBehaviour
+{
+    private EnemyVision enemyVision;
+    private float moveSpeed = 4f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    private Animator animator;
+
+    private string horizontal = "Horizontal";
+    private string vertical = "Vertical";
+    private string speed = "Speed";
+    private string isMoving = "IsMoving";
+
+    void Awake()
+    {
+        animator = transform.GetComponent<Animator>();
+        enemyVision = transform.GetComponent<EnemyVision>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (enemyVision.CanSeePlayer)
+        //{
+            animator.SetFloat(horizontal, enemyVision.DirectionVector.x);
+            animator.SetFloat(vertical, enemyVision.DirectionVector.y);
+            animator.SetFloat(speed, enemyVision.DirectionVector.magnitude);
+            
+       // }
+        animator.SetBool(isMoving, enemyVision.CanSeePlayer);
+
+
+
+    }
+
+    void FixedUpdate()
+    {
+        if (enemyVision.CanSeePlayer)
+        {
+            rb.MovePosition(rb.position + enemyVision.DirectionVector * moveSpeed * Time.fixedDeltaTime);
+        }
+        //rb.AddForce(movement * moveSpeed * Time.fixedDeltaTime, ForceMode2D.Force);
+    }
+}
